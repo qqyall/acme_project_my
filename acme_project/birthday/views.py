f53e6bad6b034c28601f8c387b19805cc2c80e70
Румянцев Савелий <radogask9rsd@gmail.com>
2024-02-02 19:38:49 +0500

    Create acme_proj

---
+++ acme_project/birthday/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+
+from .forms import BirthdayForm
+from .models import Birthday
+from .utils import calculate_birthday_countdown
+
+
+def birthday(request, pk=None):
+    if pk:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
+
+    form = BirthdayForm(request.POST or None, instance=instance)
+    context = {'form': form}
+    if form.is_valid():
+        form.save()
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday'])
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    birthdays = Birthday.objects.all()
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
+
+
+def delete_birthday(request, pk):
+    instance = get_object_or_404(Birthday, pk=pk)
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('/birthday:list')
+    return render(request, 'birthday/birthday.html', context)
